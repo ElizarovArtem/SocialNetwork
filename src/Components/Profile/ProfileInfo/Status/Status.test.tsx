6406a425b42792db56c24986bb6de2265b342052
ElizarovArtem <elizarovskiyklach@yandex.ru>
2021-01-19 22:47:58 +0300

    make tests for Status component

---
+++ src/Components/Profile/ProfileInfo/Status/Status.test.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {create} from "react-test-renderer";
+import {Status} from "./Status";
+
+describe("Status component", () => {
+    test("status in Status should be correct", () => {
+        const component = create(<Status status={"MaBoyyy"} updateStatusThunk={() => {
+        }}/>);
+        const instance = component.getInstance();
+        if (instance)
+            expect(instance.props.status).toBe("MaBoyyy");
+    });
+    test("<span> should be displayed", () => {
+        const component = create(<Status status={"MaBoyyy"} updateStatusThunk={() => {
+        }}/>);
+        const instance = component.root;
+
+        const span = instance.findByType('span')
+        expect(span).toBeDefined();
+    });
+    test("<span> should be displayed with correct status", () => {
+        const component = create(<Status status={"MaBoyyy"} updateStatusThunk={() => {
+        }}/>);
+        const instance = component.root;
+
+        const span = instance.findByType('span')
+        expect(span.children[0]).toBe("MaBoyyy");
+    });
+    test("<span> should be changed with <input> after double click", () => {
+        const component = create(<Status status={"MaBoyyy"} updateStatusThunk={() => {
+        }}/>);
+        const instance = component.root;
+        const span = instance.findByType('span')
+        span.props.onDoubleClick()
+        const input = instance.findByType('input')
+        expect(input.props.value).toBe("MaBoyyy");
+    });
+    test(" <input> shouldn't be defined", () => {
+        const component = create(<Status status={"MaBoyyy"} updateStatusThunk={() => {
+        }}/>);
+        const instance = component.root;
+
+        expect(() => {
+            const input = instance.findByType('input')
+        }).toThrow();
+    });
+    test("Status's callback should be called", () => {
+        const mockCallback = jest.fn()
+        const component = create(<Status status={"MaBoyyy"} updateStatusThunk={mockCallback}/>);
+        const instance = component.getInstance()
+        //@ts-ignore
+        instance.activateViewMode()
+        expect(mockCallback.mock.calls.length).toBe(1)
+    });
+
+});
