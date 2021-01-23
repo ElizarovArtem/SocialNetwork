8284063cb051067bf3e03bad573e770320a64cdb
ElizarovArtem <elizarovskiyklach@yandex.ru>
2021-01-23 19:46:31 +0300

    Updates

---
+++ precache-manifest.a88f187f9d5869449f2776a2f2b0befc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20bd8dab377102bc1e78a7cb86a442d",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "ca5d68fcbc6beceaffb7",
+    "url": "/SocialNetwork/static/css/3.797dba6a.chunk.css"
+  },
+  {
+    "revision": "7eb97e036355d0e6cce3",
+    "url": "/SocialNetwork/static/css/4.9146c7fe.chunk.css"
+  },
+  {
+    "revision": "72c57acf43927d04bac4",
+    "url": "/SocialNetwork/static/css/main.f5025ad2.chunk.css"
+  },
+  {
+    "revision": "92b36551575bce52c53a",
+    "url": "/SocialNetwork/static/js/2.9563ebb2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SocialNetwork/static/js/2.9563ebb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5d68fcbc6beceaffb7",
+    "url": "/SocialNetwork/static/js/3.2d42b377.chunk.js"
+  },
+  {
+    "revision": "7eb97e036355d0e6cce3",
+    "url": "/SocialNetwork/static/js/4.965d3efa.chunk.js"
+  },
+  {
+    "revision": "72c57acf43927d04bac4",
+    "url": "/SocialNetwork/static/js/main.c68f5c20.chunk.js"
+  },
+  {
+    "revision": "86d02a9b3f0c1b9f4705",
+    "url": "/SocialNetwork/static/js/runtime-main.b47dcb2c.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/SocialNetwork/static/media/preloader.26ef8bb4.gif"
+  },
+  {
+    "revision": "6f99ea5782a2fbb58946a65f4552e697",
+    "url": "/SocialNetwork/static/media/user-profile.6f99ea57.png"
+  }
+]);
