7ee6e9686af696382815323bef4a3c62a9207fd3
ElizarovArtem <elizarovskiyklach@yandex.ru>
2021-02-08 03:18:29 +0300

    change to BrowserRouter

---
+++ precache-manifest.0335088f03adeb01504a4efc40707e2e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cff516fffc5069f64d4fd51aa664dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0eaa0653ed817101bc4",
+    "url": "/static/css/3.644dad3b.chunk.css"
+  },
+  {
+    "revision": "51ae036e5f177549a54d",
+    "url": "/static/css/4.9146c7fe.chunk.css"
+  },
+  {
+    "revision": "1cac0ffa599b19c2a7e9",
+    "url": "/static/css/main.494c8559.chunk.css"
+  },
+  {
+    "revision": "0cf6c22547f88995e78a",
+    "url": "/static/js/2.b3262006.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b3262006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0eaa0653ed817101bc4",
+    "url": "/static/js/3.5d906801.chunk.js"
+  },
+  {
+    "revision": "51ae036e5f177549a54d",
+    "url": "/static/js/4.bafb96cc.chunk.js"
+  },
+  {
+    "revision": "1cac0ffa599b19c2a7e9",
+    "url": "/static/js/main.8a2c9127.chunk.js"
+  },
+  {
+    "revision": "9d5a7aefc7f345f8284e",
+    "url": "/static/js/runtime-main.439b6f11.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/static/media/preloader.26ef8bb4.gif"
+  },
+  {
+    "revision": "6f99ea5782a2fbb58946a65f4552e697",
+    "url": "/static/media/user-profile.6f99ea57.png"
+  }
+]);
--- src/utils/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const changeObjectInArray = (items: Array<any>, itemId: number, changedProp: Object) => {
-    return items.map(u => {
-        if (u.id === itemId) {
-            return {...u, ...changedProp}
-        }
-        return u;
-    })
-}
